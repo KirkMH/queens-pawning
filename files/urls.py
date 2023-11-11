0fbdf10f6b05582431e837d6d8bc379d8bfa0bcc
Kirk M. Hilario <kirkhilario@gmail.com>
2023-11-11 09:57:02 +0800

    Updated URL patterns.

--- files/urls.py
+++ files/urls.py
@@ -3,19 +3,19 @@ from . import views
 
 
 urlpatterns = [
-    path('client', views.client_list, name='client_list'),
-    path('client/dt', views.ClientDTListView.as_view(), name='client_dtlist'),
-    path('client/new', views.ClientCreateView.as_view(), name='new_client'),
-    path('client/<int:pk>/edit',
+    path('clients', views.client_list, name='client_list'),
+    path('clients/dt', views.ClientDTListView.as_view(), name='client_dtlist'),
+    path('clients/new', views.ClientCreateView.as_view(), name='new_client'),
+    path('clients/<int:pk>/edit',
          views.ClientUpdateView.as_view(), name='edit_client'),
 
 
-    path('expense/category', views.expense_category_list,
+    path('expenses/categories', views.expense_category_list,
          name='expense_category_list'),
-    path('expense/category/dt', views.ExpenseCategoryDTListView.as_view(),
+    path('expenses/categories/dt', views.ExpenseCategoryDTListView.as_view(),
          name='expense_category_dtlist'),
-    path('expense/category/new', views.ExpenseCategoryCreateView.as_view(),
+    path('expenses/categories/new', views.ExpenseCategoryCreateView.as_view(),
          name='new_expense_category'),
-    path('expense/category/<int:pk>/edit',
+    path('expenses/categories/<int:pk>/edit',
          views.ExpenseCategoryUpdateView.as_view(), name='edit_expense_category'),
 ]
