39a9e8087478c0773c4e4f16d5c5c7c397087556
Kirk M. Hilario <kirkhilario@gmail.com>
2023-12-08 20:55:41 +0800

    Added the Pawn model to pawn app.

--- pawn/models.py
+++ pawn/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from simple_history.models import HistoricalRecords
 
-# Create your models here.
+from files.models import Client
+
+
+class Inventory(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().exclude(status__in=['REDEEMED', 'AUCTIONED'])
+
+
+class Pawn(models.Model):
+    ACTIVE = 'ACTIVE'
+    RENEWED = 'RENEWED'
+    REDEEMED = 'REDEEMED'
+    MATURED = 'MATURED'
+    EXPIRED = 'EXPIRED'
+    AUCTIONED = 'AUCTIONED'
+    STATUS = [
+        (ACTIVE, _('Active')),
+        (RENEWED, _('Renewed')),
+        (REDEEMED, _('Redeemed')),
+        (MATURED, _('Matured')),
+        (EXPIRED, _('Expired')),
+        (AUCTIONED, _('Auctioned'))
+    ]
+
+    date = models.DateTimeField(auto_now_add=True)
+    client = models.ForeignKey(
+        Client,
+        on_delete=models.CASCADE,
+        null=False, blank=False
+    )
+    description = models.CharField(
+        _('Description'),
+        max_length=255,
+        null=False, blank=False
+    )
+    principal = models.DecimalField(
+        _('Principal'),
+        max_digits=10,
+        decimal_places=2,
+        null=False, blank=False
+    )
+    service_charge = models.DecimalField(
+        _('Service Charge'),
+        max_digits=10,
+        decimal_places=2,
+        null=False, blank=False
+    )
+    advance_interest = models.DecimalField(
+        _('Advance Interest'),
+        max_digits=10,
+        decimal_places=2,
+        null=False, blank=False
+    )
+    net_proceeds = models.DecimalField(
+        _('Net Proceeds'),
+        max_digits=10,
+        decimal_places=2,
+        null=False, blank=False
+    )
+    status = models.CharField(
+        _('Status'),
+        max_length=20,
+        choices=STATUS,
+        default=ACTIVE,
+        null=False, blank=False
+    )
+    status_updated_on = models.DateTimeField(auto_now=True)
+
+    history = HistoricalRecords()
+    Inventory = Inventory()
